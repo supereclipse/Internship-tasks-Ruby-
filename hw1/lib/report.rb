e1906b7383f04cf28ad0fcf12fc79473583c57b4
Arseniy Kuzmenkov <ars20022002@mail.ru>
2022-04-23 14:33:21 +0300

    moved sort

--- hw1/lib/report.rb
+++ hw1/lib/report.rb
@@ -12,7 +12,7 @@ class Report
   end
 
   def expensive(n)
-    cheap(n).reverse
+    @vm_list.map { |vm| [vm.id, VMCost.new(vm, @price_list, @volume_list.find { |vol| vol.vm_id == vm.id }).cost_full] }.sort_by { |_id, value| value }.last(n).reverse
   end
 
   def cheap(n)
@@ -21,17 +21,20 @@ class Report
 
   # Выводит n ВМ с наибольшим обьемом указанного параметра (cpu,ram,hdd_capacity)
   def most_capacity_of_type(n, type)
-    @vm_list.map { |vm| [vm.id, vm.send(type.to_sym).to_i] }.sort_by { |_id, value| value }.last(n).reverse
+    sort_by_value(@vm_list.map { |vm| [vm.id, vm.send(type.to_sym).to_i] }, n)
   end
 
   # Выводит n ВМ с наибольшим количеством дисков опр. типа
   def e_volume_amount(n, type = 'All')
-    @volume_list.map { |extra| [extra.vm_id, extra.get_amount_of_type(type)] }.sort_by { |_id, value| value }.last(n).reverse
+    sort_by_value(@volume_list.map { |extra| [extra.vm_id, extra.get_amount_of_type(type)] }, n)
   end
 
   # Выводит n ВМ с наибольшим обьемом дисков опр. типа
   def e_volume_volume(n, type = 'All')
-    @volume_list.map { |extra| [extra.vm_id, extra.get_volume_of_type(type)] }.sort_by { |_id, value| value }.last(n).reverse
+    sort_by_value(@volume_list.map { |extra| [extra.vm_id, extra.get_volume_of_type(type)] }, n)
   end
 
+  def sort_by_value (list, n)
+    list.sort_by { |_id, value| value }.last(n).reverse
+  end
 end
